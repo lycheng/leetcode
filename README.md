cf62e602d1acd60634b811443687986bf257b1f2
lycheng <lycheng997@gmail.com>
2014-04-20 01:20:07 +0800

    singleNumber

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ leetcode AC codes
 problems: http://oj.leetcode.com/problems/
 
 
-#### P1
+#### reverseWords
 
 提交了四次，第一次没有考虑到前导的空格问题，第二次没有考虑到中间的多余的空格。
 
 第三次使用了 ```strip``` 之后再用 ```' '.join(s.split())``` 来清理多余的空格
 
 第四次发现其实直接用 ```' '.join(s.split())``` 就好了
+
+
+### singleNumber
+
+a ^ b ^ a = b
--- p01_p10.py
+++ p01_p10.py
@@ -15,11 +15,20 @@ class Solution(object):
         words = [word[::-1] for word in words]
         return " ".join(words)[::-1]
 
+    def singleNumber(self, li):
+        ''' return the single num in array
+        @param A, a list of integer
+        @return an integer
+        '''
+        rv = reduce(lambda x, y: x ^ y, li)
+        return rv
+
 
 
 def run():
     o = Solution()
-    print o.reverseWords("    the sky is       blue")
+    # print o.reverseWords("    the sky is       blue")
+    print o.singleNumber([1, 2, 3, 4, 5, 3, 2, 1, 4])
 
 
 if __name__ == "__main__":
