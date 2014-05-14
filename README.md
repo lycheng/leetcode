637663450e73c0e3c864db678a776a496a2d8710
lycheng <lycheng997@gmail.com>
2014-05-15 00:13:14 +0800

    update

--- README.md
+++ README.md
@@ -8,28 +8,19 @@ problems: http://oj.leetcode.com/problems/
 #### part1
  - reverseWords
   - 多注意处理空格
-
  - singleNumber
   - a ^ b ^ a = b
-
  - maxDepth
   - DFS
-
  - isSameTree
   - DFS
-
  - reverse number
   -这道题用 python 略犯规啊，它好心提示一点用都没有
-
  - hasCycle
   - 前面一个指针一次跳两个，后面一个指针一次跳一个，总会相遇
-
  - preorder && inorder
-
  - searchInsert
-
  - deleteDuplicates
-
  - climbStairs
   - 第一次用递归做超时，后面想了下，大概的思路是：到达某一个 n 可能的步数就是到达 n - 1 和到达 n - 2 的和
 
@@ -37,3 +28,6 @@ problems: http://oj.leetcode.com/problems/
  - maxSubArray
   - sum[i+1] = max(a[i+1], sum[i] + a[i+1])
   - result = max(result, sum[i])
+ - removeElement
+ - twoSum
+  - 错了挺多次，最开始直接用 index 来找位置，没考虑到重复元素
--- part2.py
+++ part2.py
@@ -19,3 +19,43 @@ class Solution(object):
                 rv = cur_sum
 
         return rv
+
+    def removeElement(self, A, elem):
+        index = 0
+        for num in A:
+            if num == elem:
+                continue
+            A[index] = num
+            index += 1
+
+        return index
+
+    def twoSum(self, num, target):
+        nums = sorted(num)
+
+        left = 0
+        right = len(num) - 1
+
+        while left < right:
+            cur_sum = nums[left] + nums[right]
+            if cur_sum > target:
+                right -= 1
+                continue
+            elif cur_sum < target:
+                left += 1
+                continue
+            if cur_sum == target:
+                break
+
+        result = []
+        for index in range(len(num)):
+            if num[index] == nums[left]:
+                result.append(index + 1)
+            elif num[index] == nums[right]:
+                result.append(index + 1)
+
+            if len(result) == 2:
+                break
+
+        return tuple(result)
+
