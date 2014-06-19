de6c987c046a0621b0e5e37c04ce3818bae322da
lycheng <lycheng997@gmail.com>
2014-06-20 00:10:21 +0800

    update

--- part7.py
+++ part7.py
@@ -5,6 +5,7 @@ __author__ = 'lycheng'
 __email__ = "lycheng997@gmail.com"
 
 from public import linked_to_list, list_to_linked
+from public import TreeNode
 
 import unittest
 
@@ -64,6 +65,96 @@ class Solution(unittest.TestCase):
         rv = self.removeNthFromEnd(head, 1)
         self.assertEqual(linked_to_list(rv), [])
 
+    def removeDuplicates(self, A):
+        length = len(A)
+        if length <= 2:
+            return length
+
+        index = 1
+        for behind in range(2, length):
+            if A[behind] == A[index] and A[behind] == A[index-1]:
+                continue
+            index += 1
+            A[index] = A[behind]
+        return index + 1
+
+    def test_removeDuplicates(self):
+        self.assertEqual(self.removeDuplicates([1, 2, 3]), 3)
+        self.assertEqual(self.removeDuplicates([1, 1, 3]), 3)
+        self.assertEqual(self.removeDuplicates([1, 1, 3, 4]), 4)
+        self.assertEqual(self.removeDuplicates([1, 1, 1, 4]), 3)
+        self.assertEqual(self.removeDuplicates([1,1,1,2,2,3]), 5)
+
+    def levelOrder(self, root):
+        rv = []
+        if not root:
+            return rv
+        rv.append([root.val])
+        cur_level = [root]
+        next_level = []
+        while cur_level:
+            node = cur_level.pop(0)
+            if node.left:
+                next_level.append(node.left)
+            if node.right:
+                next_level.append(node.right)
+
+            if not cur_level:
+                if not next_level:
+                    break
+                rv.append([n.val for n in next_level])
+                cur_level = next_level
+                next_level = []
+
+        return rv
+
+    def test_levelOrder(self):
+        root = TreeNode(3)
+        root.left = TreeNode(9)
+        root.right = TreeNode(20)
+        root.right.left = TreeNode(15)
+        root.right.right = TreeNode(7)
+
+        rv = self.levelOrder(root)
+        self.assertEqual(rv, [[3],[9,20],[15,7]])
+
+    def addBinary(self, a, b):
+
+        if len(a) < len(b):
+            a, b = b, a
+
+        diff_len = len(a) - len(b)
+        if diff_len:
+            b = ('0' * diff_len) + b
+
+        carry = 0
+        result = []
+        for i in range(len(a) - 1, -1, -1):
+            r = int(a[i]) + int(b[i]) + carry
+            if r > 1:
+                result.append(str(r % 2))
+                carry = 1
+            else:
+                result.append(str(r))
+                carry = 0
+        if carry:
+            result.append('1')
+
+        return ''.join(result[::-1])
+
+
+    def test_addBinary(self):
+        r = self.addBinary('11', '110')
+        self.assertEqual(r, '1001')
+
+        r = self.addBinary('1', '1')
+        self.assertEqual(r, '10')
+
+        r = self.addBinary('11', '1')
+        self.assertEqual(r, '100')
+
+        r = self.addBinary('1111', '1111')
+        self.assertEqual(r, '11110')
 
 if __name__ == "__main__":
     unittest.main()
